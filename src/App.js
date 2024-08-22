21c216692f9e4b6508757e4db6e932e6b33a1559
SUMIT LOHAR <94884266+Loharsumit@users.noreply.github.com>
2024-08-22 17:51:57 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css'
-import img from './assets/1.jpg'
 import sl1 from './assets/slide1.jpeg'
 import sl2 from './assets/slide2.jpg'
 import sl3 from './assets/slide3.jpg'
 import sl4 from './assets/slide4.jpeg'
 import sl5 from './assets/slide5.jpg'
-// import sl6 from './assets/slide6.jpg'
-// import sl7 from './assets/slide7.jpeg'
-// import sl8 from './assets/slide8.jpeg'
-// import sl9 from './assets/slide9.jpg'
-// import sl10 from './assets/slide10.jpg'
 import style1 from './assets/style1.jpg'
 import style2 from './assets/style2.jpg'
 
@@ -20,18 +14,6 @@ function App() {
   return (
     <>
         <h1 id='heading'>AboutUs</h1>
-      <div className='container'>
-       <div className="containt">
-        <h1>Who We Are?</h1>
-        <h2>We are a team of 10 certified beauty professionals with an extensive experience known to have covered major concerts and events in town.</h2>
-        <p>We are known for the affordable and hassle-free services that we offer to all our clients who’ve been trusting us since years. So, if you are looking for a combination of reasonable and professional beauty services, you won’t regret coming to us. We’ve mastered the art of understanding client requirements and delivering the best.</p>
-       </div>
-       <div className="image">
-        <img src={img} alt="" />
-       </div>
-      </div>
-
-
       <div class="hair-style">
         <div class="hair-style-image">
             <img src={style1} alt="" />
@@ -75,13 +57,6 @@ function App() {
          <img src={sl4} alt=""/>
          <img src={sl5} alt=""/>
      </div>
-     {/* <div className="logo-slide">
-         <img src={sl6} alt=""/>
-         <img src={sl7} alt=""/>
-         <img src={sl8} alt=""/>
-         <img src={sl9} alt=""/>
-         <img src={sl10} alt=""/>
-     </div> */}
   </div>
 
     </>
